165e049428b8584ef1d5bee1a1d57f6aea971f93
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-09-09 14:33:59 -0300

    adicionando kc para a classe de cultura

--- app/src/main/java/com/agriext/willn/agriext/Entity/Culture.java
+++ app/src/main/java/com/agriext/willn/agriext/Entity/Culture.java
@@ -3,10 +3,12 @@ package com.agriext.willn.agriext.Entity;
 public class Culture {
     private String name;
     private String urlImage;
+    private double kc;
 
-    public Culture(String name, String urlImage) {
+    public Culture(String name, String urlImage, double kc) {
         this.name = name;
         this.urlImage = urlImage;
+        this.kc = kc;
     }
 
     public String getName() {
@@ -23,5 +25,13 @@ public class Culture {
 
     public void setUrlImage(String urlImage) {
         this.urlImage = urlImage;
+    }
+
+    public double getKc() {
+        return kc;
+    }
+
+    public void setKc(double kc) {
+        this.kc = kc;
     }
 }
