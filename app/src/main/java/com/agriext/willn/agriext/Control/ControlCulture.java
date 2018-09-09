2e295faaf5e500f0ffbcee05ef50889c2505bbbd
Wilton Neto <wiltonribeiro@users.noreply.github.com>
2018-09-09 14:33:26 -0300

    coeficiente das culturas compreende agora ao valor real médio
    Usando valores únicos, dados como médios, já que há valores alternantes de kc de uma cultura dependendo de seu estágio de desenvolvimento.

--- app/src/main/java/com/agriext/willn/agriext/Control/ControlCulture.java
+++ app/src/main/java/com/agriext/willn/agriext/Control/ControlCulture.java
@@ -25,9 +25,9 @@ public class ControlCulture {
 
     private List<Culture> fillCultureData(){
         List<Culture> cultures = new ArrayList<>();
-        cultures.add(new Culture("Arroz", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx9mIK7jiHgbRMTpU5YJtPotn8Sxhnpwx6aysL1WUh-tOwUl9RKQ"));
-        cultures.add(new Culture("Café", "http://www.agronovas.com.br/wp-content/uploads/2015/05/cafe_2.jpg"));
-        cultures.add(new Culture("Soja", "http://www.brasil.gov.br/economia-e-emprego/2016/01/safra-2015-2016-atingira-210-5-milhoes-de-toneladas-de-graos/img_8861.jpg/@@images/ba1a4d16-cfaa-4907-ab68-2e523f121683.jpeg"));
+        cultures.add(new Culture("Arroz", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx9mIK7jiHgbRMTpU5YJtPotn8Sxhnpwx6aysL1WUh-tOwUl9RKQ",1.05));
+        cultures.add(new Culture("Café", "http://www.agronovas.com.br/wp-content/uploads/2015/05/cafe_2.jpg",0.65));
+        cultures.add(new Culture("Soja", "http://www.brasil.gov.br/economia-e-emprego/2016/01/safra-2015-2016-atingira-210-5-milhoes-de-toneladas-de-graos/img_8861.jpg/@@images/ba1a4d16-cfaa-4907-ab68-2e523f121683.jpeg", 0.75));
 
         return  cultures;
     }
